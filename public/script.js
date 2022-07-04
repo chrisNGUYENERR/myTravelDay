bac6f38eb774662d8b91e9ed5623273031f3ee4c
Chris Nguyen <chrishiepnguyen@yahoo.com>
2022-07-04 18:20:29 -0500

    connected add user function to form

---
+++ public/script.js
@@ -0,0 +1,10 @@
+const form = document.getElementById('form')[0];
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    let data = {
+        "name": this.name.value
+    }
+})
+
+
